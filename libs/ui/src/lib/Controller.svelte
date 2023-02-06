cd10a7002f2fe372ad7212d60f67f4d79c0ce509
Eric Harris-Braun <eric@harris-braun.com>
2023-02-06 10:46:25 -0500

    clean up fade transition

--- libs/ui/src/lib/Controller.svelte
+++ libs/ui/src/lib/Controller.svelte
@@ -8,7 +8,7 @@
     import { BoardType } from './board';
     import { MaterialApp, Icon } from 'svelte-materialify';
     import { mdiArchiveArrowUp, mdiCog, mdiShapeSquarePlus } from '@mdi/js';
-  import { fade } from 'svelte/transition';
+    import { fade } from 'svelte/transition';
 
     export let boardType: BoardType = BoardType.Ludos
     export let roleName = ""
@@ -60,47 +60,51 @@
   <div class='app'>
     {#if tsStore}
       <Toolbar boardType={boardType}/>
+      <div class="transition-container">
       {#if boardList && $boardList.boards.length == 0}
-      <div class="welcome-text" transition:fade>
-        <p>
-        The <a href="https://docs.google.com/document/d/1HBNgIooElD5widCuX9XmiOzbVIpEF5XXH67mZbnUFjo">Players of Ludos</a> had a game they played that involved creating fictional realms.
-        These realms were crafted by the players who took turns writing new spaces one-by-one and adding them to the previous ones creating a kind of map, that other players
-        could then wander through and read, almost as novel.
-        </p>
-        <p>
-          The players considered their play, like our living, a kind of dreaming.  And so when they were crafting new games they felt themselves to be awake.
-        </p>
-        <p>      
-        So, here, dear players, we offer you the chance to join into this long lost game-form. To shift between the dreaming and the waking. 
-        </p>
-        <p>
-          Click on the <Icon style="width:20px; color:black; vertical-align: bottom;" path={mdiShapeSquarePlus}></Icon> above to create your first realm.
+        <div class="welcome-text" transition:fade>
+          <p>
+          The <a href="https://docs.google.com/document/d/1HBNgIooElD5widCuX9XmiOzbVIpEF5XXH67mZbnUFjo">Players of Ludos</a> had a game they played that involved creating fictional realms.
+          These realms were crafted by the players who took turns writing new spaces one-by-one and adding them to the previous ones creating a kind of map, that other players
+          could then wander through and read, almost as novel.
+          </p>
+          <p>
+            The players considered their play, like our living, a kind of dreaming.  And so when they were crafting new games they felt themselves to be awake.
+          </p>
+          <p>      
+          So, here, dear players, we offer you the chance to join into this long lost game-form. To shift between the dreaming and the waking. 
+          </p>
+          <p>
+            Click on the <Icon style="width:20px; color:black; vertical-align: bottom;" path={mdiShapeSquarePlus}></Icon> above to create your first realm.
 
-          Click on the yellow square to edit the description of the space.  You can type in the terminal to begin your dreaming, or if you are clicky type person who remains awake and crafting, then click on a green squares to add an adjacent space.  
-          Soon you will have crafted an entire realm to share!
-        </p>
-        <p>
-        May your collective imaginations flourish and your dreams be of delight to you...
-        </p>
-      </div>
+            Click on the yellow square to edit the description of the space.  You can type in the terminal to begin your dreaming, or if you are clicky type person who remains awake and crafting, then click on a green squares to add an adjacent space.  
+            Soon you will have crafted an entire realm to share!
+          </p>
+          <p>
+          May your collective imaginations flourish and your dreams be of delight to you...
+          </p>
+        </div>
       {/if}
       {#if boardList && $boardList.boards.length > 0 && $activeBoardIndex === undefined}
-      <div class="welcome-text" transition:fade>
-        <p>Active Realms: {activeBoards.length}, Archived Realms: {archivedBoards.length}</p>
-          <p>
-            Select a realm from the dropdown above, or add a new one with the  <Icon style="width:20px; color:black; vertical-align: bottom;" path={mdiShapeSquarePlus}></Icon> button.
-          </p>
+        <div class="welcome-text" transition:fade>
+          <p>Active Realms: {activeBoards.length}, Archived Realms: {archivedBoards.length}</p>
+            <p>
+              Select a realm from the dropdown above, or add a new one with the  <Icon style="width:20px; color:black; vertical-align: bottom;" path={mdiShapeSquarePlus}></Icon> button.
+            </p>
 
-        <p>You can always edit these settings with the <Icon style="width:20px; color:black; vertical-align: bottom;" path={mdiCog}></Icon> button in the upper right when you have a realm selected. </p>
-        <p>Any realms that you have archived will appear under the <Icon style="width:20px; color:black; vertical-align: bottom;" path={mdiArchiveArrowUp}></Icon> button, and you can un-archive them by selecting them from the list.</p>
-      </div>
-    {/if}
+          <p>You can always edit these settings with the <Icon style="width:20px; color:black; vertical-align: bottom;" path={mdiCog}></Icon> button in the upper right when you have a realm selected. </p>
+          <p>Any realms that you have archived will appear under the <Icon style="width:20px; color:black; vertical-align: bottom;" path={mdiArchiveArrowUp}></Icon> button, and you can un-archive them by selecting them from the list.</p>
+        </div>
+      {/if}
 
       {#if $activeBoardIndex !== undefined}
         {#if $activeBoardType === BoardType.Ludos}
+          <div transition:fade>
           <BoardPane on:requestChange={(event) => {tsStore.boardList.requestBoardChanges($activeBoardIndex,event.detail)}}/>
+          </div>
         {/if}
       {/if}
+    </div>
     {:else}
       <div class="loading"><div class="loader"></div></div>
     {/if}
@@ -151,5 +155,16 @@
   @keyframes spin {
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
+  }
+
+  .transition-container {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr;
+  }
+
+  .transition-container > * {
+    grid-row: 1;
+    grid-column: 1;
   }
 </style>
